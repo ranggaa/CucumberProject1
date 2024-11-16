0aed12d02c2cf128196f04c865ec410347c0ee44
lalatendu11 <lalatendung@gmail.com>
2024-11-16 09:09:34 +0530

    New Class

---
+++ Cucumber_ERP/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,20 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int x=345;
+	int y=4566;
+	int z=x+y;
+	System.out.println(z);
+			
+}
+public void div()
+{
+	int x=564;
+	int y=5;
+	int z=x/y;
+	System.out.println(z);
+			
+}
+}
